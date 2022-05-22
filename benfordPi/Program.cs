61f33e912168785db4c65c8822cb78bf2dc2b541
Skalefou <45128700+Skalefou@users.noreply.github.com>
2022-05-22 21:38:48 +0200

    Add benfordPi

---
+++ benfordPi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BenfordPi
+{
+    class program
+    {
+        static public void Main(String[] args)
+        {
+            Console.WriteLine("Salut pepito !");
+        }
+    }
+}
