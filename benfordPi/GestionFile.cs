c8ce3d8d548f9423d23e7520e8a393734644102f
Skalefou <45128700+Skalefou@users.noreply.github.com>
2022-05-26 18:28:49 +0200

    Add Benford Pi with C#

--- benfordPi/GestionFile.cs
+++ benfordPi/GestionFile.cs
@@ -5,10 +5,15 @@ namespace piBenford
     class GestionFile
     {
         private StreamReader sr = new StreamReader("pi.txt");
+
+        public bool end()
+        {
+            return sr.Peek() == -1;
+        }
         public int read()
         {
-           int a = sr.Read();
-           return a;
+            int a = sr.Read();
+            return a - 48;
         }
 
         public bool isOver()
--- benfordPi/Program.cs
+++ benfordPi/Program.cs
@@ -9,9 +9,19 @@ namespace piBenford
 		{
 			Console.WriteLine("PiBenford by Skalefou\nOccurrence of digits in the number Pi");
 			GestionFile file = new GestionFile();
-			char f;
-			//while()
-				f = Convert.ToChar(file.read());
+			int[] number = new int[10];
+			Array.Fill(number, 0);
+			while (!file.end())
+			{
+				int f = file.read();
+				if(f >= 0)
+					number[f]++;
+			}
+			for(int i = 0; i < 10; i++)
+			{
+				Console.WriteLine(i + " : " + number[i]);
+			}
+			Console.Read();
 		}
 	}
 
